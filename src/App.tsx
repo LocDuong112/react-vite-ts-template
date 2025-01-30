48fbb13d213dc29bb8f4f239adeb9d88df3c36e0
LocDuong112 <duongminhloc201@gmail.com>
2025-01-30 21:36:32 +0700

    add props and todolist

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,32 @@ import InputTodo from "./todo/input.todo"
 
 function App() {
 
+  const name = "Loc";
+  const age = 25;
+  const info = {
+    gender: "Male",
+    address: "Home"
+  }
+
+  const todos = ["todo 1", "todo 2", "todo 3"];
+
   return (
     <div>
-      <InputTodo />
+      <InputTodo
+        name={name}
+        age={age}
+        info={info}
+      />
+
+      <ul>
+        {
+          todos.map((item, idx) => {
+            return (
+              <li key={idx}>{item}</li>
+            )
+          })
+        }
+      </ul>
     </div>
   )
 }
--- src/todo/input.todo.tsx
+++ src/todo/input.todo.tsx
@@ -1,8 +1,23 @@
-const InputTodo = () => {
+interface IProps {
+    name: String;
+    age: number;
+    info: {
+        gender: String;
+        address: String;
+    }
+
+    abc?: String;
+}
+
+const InputTodo = (props: IProps) => {
+
+    const { age, name } = props;
 
     return (
         <div>
             <div>Add new todo</div>
+            <div>Name: {name} </div>
+            <div>Age: {age} </div>
             <div style={{ display: "flex" }}>
                 <input type="text" />
                 <button style={{ marginLeft: "5px" }} type="button">Save</button>
