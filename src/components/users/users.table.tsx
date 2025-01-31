3203a820cd779b23e6a96de33c8a76c365359efa
LocDuong112 <duongminhloc201@gmail.com>
2025-01-31 19:25:16 +0700

    add user table page and wait for BE

---
+++ src/components/users/users.table.tsx
@@ -0,0 +1,28 @@
+import "../../styles/users.css"
+
+const UsersTable = () => {
+    return (
+        <div>
+            <h2>Users table</h2>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>Head 1</th>
+                        <th>Head 2</th>
+                        <th>Head 3</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Data 1 1</td>
+                        <td>Data 1 2</td>
+                        <td>Data 1 3</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default UsersTable;
